79cad99068588e9631440fd054c7a86323f380fb
ENux-Distro <emir73503@gmail.com>
2025-11-22 08:02:55 -0500

    Update 00-welcome.sh

--- 00-welcome.sh
+++ 00-welcome.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-source /lib/ui.sh
+# Ensure flat repo: ui.sh in same folder
+source "$(dirname "$0")/ui.sh"
 
-title "Welcome to ENux Installer"
+echo "-----Welcome to ENux Installer!-----"
+
 echo "This installer will guide you through installing ENux on your system."
 echo "Press ENTER to continue..."
 read
