a1c85d2c5fa70feaeffcd48e0bc426eb4f55cb6c
ENux-Distro <emir73503@gmail.com>
2025-11-22 08:04:47 -0500

    Update 20-mount.sh

--- 20-mount.sh
+++ 20-mount.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-source /lib/ui.sh
+# Flat repo: ui.sh in same folder
+source "$(dirname "$0")/ui.sh"
 
-echo: "-----Mounting Partitions-----"
+echo "-----Mounting Partitions-----"
 
+# Root
 ROOT_PART=$(cat /tmp/enux-root)
 mount "$ROOT_PART" /mnt
 
+# EFI
 EFI_PART=$(cat /tmp/enux-efi)
 if [ -n "$EFI_PART" ]; then
     mkdir -p /mnt/boot/efi
     mount "$EFI_PART" /mnt/boot/efi
 fi
 
+# Swap
 SWAP_PART=$(cat /tmp/enux-swap)
 swapon "$SWAP_PART"
 
