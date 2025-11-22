8d1c400a66f115ab1936970abda718351b10f9c0
ENux-Distro <emir73503@gmail.com>
2025-11-22 08:07:01 -0500

    Update 50-grub.sh

--- 50-grub.sh
+++ 50-grub.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-source /lib/ui.sh
+source "$(dirname "$0")/ui.sh"
 
 echo "-----Installing GRUB Bootloader-----"
 
-# Detect which disk to install GRUB to
-DISK=$(cat /tmp/enux-root | sed 's/[0-9]*$//') # strip partition number
+# Detect which disk to install GRUB to (strip partition number from root)
+DISK=$(cat /tmp/enux-root | sed 's/[0-9]*$//')
 echo "Installing GRUB to $DISK..."
 
-# Make sure virtual filesystems are mounted before chroot
+# Ensure virtual filesystems are mounted for chroot
 mount --types proc /proc /mnt/proc
 mount --rbind /sys /mnt/sys
 mount --rbind /dev /mnt/dev
 mount --rbind /run /mnt/run
 
-# Install GRUB
-chroot /mnt /bin/bash -c "grub-install $DISK && update-grub"
-
+# Install GRUB (handles BIOS/UEFI if grub-pc/efi is installed)
+chroot /mnt /bin/bash -c "
+apt update
+apt install -y grub-pc grub-efi
+grub-install $DISK
+update-grub
+"
 
 echo "GRUB installation complete."
 read -p "Press ENTER to continue..."
+
